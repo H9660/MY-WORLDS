74060b44fbfba6425c47c8f78650b5e77596a5f4
HUSSAIN LOHAWALA <lohahussain0@gmail.com>
2021-10-24 19:59:17 +0530

    mind_the_key

---
+++ C++ PROJECTS/guess_the_key.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string.h>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    system("cls");
+    srand(time(NULL));
+    int s, i = 0, flag = 0, ch, totalplays = 0, wins = 0;
+    cout << "RULES ARE SIMPLE: JUST REMEMBER THE CODE DISPLAYED BEFORE YOU AND THEN TYPE IT AFTER IT DISAPPER."
+         << "\n";
+    cout << "LETS PLAY!"
+         << "\n";
+    do
+    {
+        wins = 0;
+        totalplays = 0;
+        flag=0;
+        totalplays++;
+        cout << "THE CODE IS:"
+             << "\n";
+        s = rand() % 6;
+        int *arr = new int[s];
+        int *user = new int[s];
+        while (i < s)
+        {
+            arr[i] = rand() % 100;
+            cout << arr[i] << " ";
+            i++;
+        }
+        i = 0;
+
+        Sleep(5000);
+        system("cls");
+        cout << "\n";
+        cout << "NOW WRITE THE CODE HERE: " << endl;
+        while (i < s)
+        {
+            cin >> user[i];
+            if (user[i] != arr[i])
+                flag = 1;
+            i++;
+        }
+        if (flag == 1)
+        {
+            cout << "WRONG CODE!"
+                 << "\n";
+            wins++;
+        }
+
+        else
+        {
+            cout<<"HURRAH! YOU WON!"<<"\n";
+            wins++;
+        }
+
+        i = 0;
+        cout << "WANT TO PLAY AGAIN(1/0)?"
+             << "\n";
+        cin >> ch;
+   
+    } while (ch == 1);
+
+    if (ch == 0)
+    {
+        cout << "THANKS FOR PLAYING! " << wins << " OF YOUR ANSWERS WERE CORRECT OUT OF " << totalplays;
+    }
+    return 0;
+}
